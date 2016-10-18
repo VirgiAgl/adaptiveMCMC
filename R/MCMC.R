763fea3733ea3cd8b170bc6ef9b536540f1a0c1e
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-10-18 15:01:15 +0100

    Remove old mcmc functions

--- R/MCMC.R
+++ R/MCMC.R
@@ -3,21 +3,23 @@ library(Matrix)
 library(mvtnorm)
 library(CVTuningCov)
 library(corpcor)
+library(PDSCE)
+library(ggplot2)
+library(gridExtra)
+
 
 ############################################################################
 # Still to implement
 # ------------------
-# Second algorithm for adaptation
 # Burn in
-# Normal MH acceptance probability
 # Do something with the optimal alpha
 # Suboptimality factor
 # Implement and test higher dimensional problem
 # Speed up code
-# Check updating acceptance forumla for AM3 and 4
 ############################################################################
 
-mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard", t_adapt = Inf){
+
+mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="Sample covariance", t_adapt = Inf){
 
   start = Sys.time()
 
@@ -29,6 +31,10 @@ mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard
       "iterations.")
   cat("\nAdaptation algorithm is: ",
       adapt,
+      "."
+  )
+  cat("\nCovariance matrix estimator is: ",
+      cov_estimator,
       "."
   )
 
@@ -43,7 +49,10 @@ mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard
   #####################################################################
   X = matrix(NA, n_iter+1, d)  #  Initialise matrix to store the markov chain
   X[1,] = x_1  #  Store the first step of the markov chain
-  acceptances = 0  #  Initialise counter for accepted states
+  acceptances = c(rep(NA,n_iter+1))  #  Initialise vector to store the number of accepted states
+  acceptances[1] = 0  #  Store the first number of accepted states
+  expected_values = matrix(NA,n_iter+1,d)  #  Initialise matrix to store the expected valeus
+  expected_values[1,] = sapply(x_1,mean)  #  Store the first step expected value
 
   #####################################################################
   # Some initialisation needed for AM adaptation
@@ -55,7 +64,9 @@ mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard
     #sigma = nearPD(sigma)$mat
     mean_X = matrix(NA, n_iter+1, d) #  Initialise matrix to store means
     mean_X[1,] = x_1 #  First mean is first step of chain
-  }
+    s_d = (2.4)^2/d  #  The scaling parameter
+    beta = 0.05
+    }
   if (adapt=="None") {
     sigma = diag(1,d,d) #  No correlation in proposal
   }
@@ -89,34 +100,49 @@ mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard
       }
 
       if (i == 2*d+1) {
-        if (cov_estimator=="standard"){
+        if (cov_estimator=="Sample covariance"){
           X_until_now = X[1:(i-1),] #the rows of X that we have filled so far
           sigma = s_d*cov(X_until_now)
           sigma = nearPD(sigma)$mat
           Y = (beta*q_norm(mu=X[i-1,], sigma=diag(d))) + ((1-beta)*q_norm(mu=X[i-1,],sigma=sigma))
         }
-        if (cov_estimator=="shrinkage"){
+        if (cov_estimator=="Shrinkage estimator"){
           X_until_now = X[1:(i-1),]
           covariance=cor.shrink(X_until_now)
+          lambda = attr(covariance,"lambda")
           sigma=s_d*covariance
           sigma=sigma[(1:d),(1:d)]
           sigma = nearPD(sigma)$mat
           Y = (beta*q_norm(mu=X[i-1,], sigma=diag(d))) + ((1-beta)*q_norm(mu=X[i-1,],sigma=sigma))
         }
-        if (cov_estimator=="threesholding"){
+        if (cov_estimator=="Thresholding estimator"){
           X_until_now = X[1:(i-1),]
-          covariance=cor.shrink(X_until_now)
+          covariance=band.chol(X_until_now,k=4)
           sigma=s_d*covariance
-          sigma=sigma[(1:d),(1:d)]
           sigma = nearPD(sigma)$mat
           Y = (beta*q_norm(mu=X[i-1,], sigma=diag(d))) + ((1-beta)*q_norm(mu=X[i-1,],sigma=sigma))
         }
       }
 
       if (i > 2*d+1) {
+        if (cov_estimator=="Sample covariance") {
         sigma = ((i-2)/(i-1))*sigma + (s_d/(i-1))*(((i-1)*mean_X[i-2,]%*%t(mean_X[i-2,]))-(i*mean_X[i-1,]%*%t(mean_X[i-1,]))+mean_X[i-1,]%*%t(mean_X[i-1,]))
         sigma = nearPD(sigma)$mat
         Y = (beta * q_norm(mu=X[i-1,], sigma=diag(1,d,d))) + ((1-beta) * q_norm(mu=X[i-1,], sigma=sigma))
+        }
+        if (cov_estimator=="Shrinkage estimator") {
+          sigma = ((i-2)/(i-1))*sigma + (s_d/(i-1)*(1-lambda))*(((i-1)*mean_X[i-2,]%*%t(mean_X[i-2,]))-(i*mean_X[i-1,]%*%t(mean_X[i-1,]))+mean_X[i-1,]%*%t(mean_X[i-1,]))+((lambda*s_d*diag(d))/(i-1))
+          sigma = nearPD(sigma)$mat
+          Y = (beta * q_norm(mu=X[i-1,], sigma=diag(1,d,d))) + ((1-beta) * q_norm(mu=X[i-1,], sigma=sigma))
+        }
+        if (cov_estimator=="Thresholding estimator") {
+          X_until_now = X[1:(i-1),]
+          covariance=band.chol(X_until_now,k=4)
+          sigma=s_d*covariance
+          sigma = nearPD(sigma)$mat
+          Y = (beta * q_norm(mu=X[i-1,], sigma=diag(1,d,d))) + ((1-beta) * q_norm(mu=X[i-1,], sigma=sigma))
+        }
+
       }
       numerator = target(Y)
       denom = target(X[i-1,])
@@ -125,31 +151,59 @@ mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard
 
     if (adapt=="None") {
       Y = q_norm(mu=X[i-1,], sigma=diag(d)) #proposal distribution sampled at the current point
-      numerator = target(Y) #*************NEED TO CHANGE THIS************
-      denom = target(X[i-1,])
+      numerator = target(Y)*q_norm(X[i-1,],sigma=diag(d)) #*************NEED TO CHANGE THIS************
+      denom = target(X[i-1,])*q_norm(Y,sigma=diag(d))
     }
 
     alpha = min(1,numerator/denom)
 
     if(runif(1) < alpha) {
       X[i,] = Y
-      acceptances = acceptances + 1
-    } else{
+      acceptances[i] = acceptances[i-1] + 1
+      } else{
       X[i,] = X[i-1,]
+      acceptances[i] = acceptances[i-1]
     }
 
     if (adapt=="AM") {
       mean_X[i,] = ((mean_X[i-1,] * (i-1)) + X[i,])/i
 
-      if (i == t_adapt) { #the first adaptation step
-        X_until_now = X[1:i,] #the rows of X that we have filled so far
-        sigma = s_d * ( cov(X_until_now) + epsilon * diag(d))
+      if (i == t_adapt) {  #the first adaptation step
+         #the rows of X that we have filled so far
+        X_until_now = X[1:i,]
+        if (cov_estimator=="Sample covariance") {
+          sigma = s_d * ( cov(X_until_now) + epsilon * diag(d))
+        }
+        if (cov_estimator=="Shrinkage estimator") {
+          covariance=cor.shrink(X_until_now)
+          lambda = attr(covariance,"lambda")
+          sigma=s_d*covariance
+          sigma=sigma[(1:d),(1:d)]
+          sigma = nearPD(sigma)$mat
+          }
+        if (cov_estimator=="Thresholding estimator") {
+          covariance=band.chol(X_until_now,k=4)
+          sigma=s_d*covariance
+          sigma = nearPD(sigma)$mat
+          }
       }
-      if (i > t_adapt) { #the later adaptation step
-        X_until_now = X[1:i,] #the rows of X that we have filled so far
-        #recursive sigma
-        sigma = ((i-2)/(i-1))*sigma + (s_d/(i-1))*(((i-1)*mean_X[i-2,]%*%t(mean_X[i-2,]))-(i*mean_X[i-1,]%*%t(mean_X[i-1,]))+mean_X[i-1,]%*%t(mean_X[i-1,])+epsilon*diag(d))
-        #sigma = nearPD(sigma)$mat
+      if (i > t_adapt) {       #the later adaptation step
+        X_until_now = X[1:i,]  #the rows of X that we have filled so far
+
+        if (cov_estimator=="Sample covariance") {
+          #recursive sigma
+          sigma = ((i-2)/(i-1))*sigma + (s_d/(i-1))*(((i-1)*mean_X[i-2,]%*%t(mean_X[i-2,]))-(i*mean_X[i-1,]%*%t(mean_X[i-1,]))+mean_X[i-1,]%*%t(mean_X[i-1,])+epsilon*diag(d))
+          sigma = nearPD(sigma)$mat
+        }
+        if (cov_estimator=="Shrinkage estimator") {
+          sigma = ((i-2)/(i-1))*sigma + (s_d/(i-1)*(1-lambda))*(((i-1)*mean_X[i-2,]%*%t(mean_X[i-2,]))-(i*mean_X[i-1,]%*%t(mean_X[i-1,]))+mean_X[i-1,]%*%t(mean_X[i-1,]))+((lambda*s_d*diag(d))/(i-1))
+          sigma = nearPD(sigma)$mat
+        }
+        if (cov_estimator=="Thresholding estimator") {
+          covariance=band.chol(X_until_now,k=4)
+          sigma=s_d*covariance
+          sigma = nearPD(sigma)$mat
+        }
       }
     }
 
@@ -157,13 +211,17 @@ mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard
       mean_X[i,] = ((mean_X[i-1,] * (i-1)) + X[i,])/i
     }
 
+    X_until_now = X[1:i,]
+    expected_values[i,] = apply(X_until_now,2,mean)
 
   }  #  End of loop
 
-  cat("\nMCMC finished in", Sys.time() - start, "seconds.")
-  cat("\nThe acceptance rate was: ", acceptances/n_iter)
-  return(X)
+  alpha_values<-acceptances/seq(1,length(acceptances),by=1)
 
+  cat("\nMCMC finished in", Sys.time() - start, "seconds.")
+  cat("\nThe last acceptance rate was:", acceptances[n_iter]/n_iter)
+  results = list(X=X, acceptance_rates=alpha_values, sample_mean=expected_values, n_acceptances=acceptances)
+  return(results)
 }
 
 ############################################################################
@@ -173,36 +231,168 @@ mcmc = function(target, n_iter = 100, x_1, adapt="None", cov_estimator="standard
 pi_banana = function(x, B=0.03) {
   exp(-x[1]^2/200 - 1/2*(x[2]+B*x[1]^2-100*B)^2)
 }
+pi_banana_matrix = function(x, B=0.03) {
+  exp(-x[,1]^2/200 - 1/2*(x[,2]+B*x[,1]^2-100*B)^2)
+}
 
 #3d banana
-pi_banana3 = function(x, B=0.1) {
+pi_banana3 = function(x, B=0.03) {
   exp(-(x[1]^2)/200 - (1/2)*(x[2]+B*x[1]^2-100*B)^2-(1/2)*(x[3]^2))
 }
 
+
+#d-dimensional banana
+pi_bananad = function(x, B=0.03) {
+  exp(-(x[1]^2)/200 - (1/2)*(x[2]+B*x[1]^2-100*B)^2-(1/2)*(sum(x[3:d]^2)))
+}
+
+
+# Uncorrelated Gaussian distribution  (d=2)
 pi_norm = function(x) {
   d=2
   dmvnorm(x, mean = rep(0, d), sigma = matrix(c(1,0,0,1),2,2), log = FALSE)
 }
 
+# Correlated Gaussian distribution  (d=8)
+# Generating a random positive-definite matrix
+Posdef <- function (n, ev = runif(n, 0, 10)) {
+  Z <- matrix(ncol=n, rnorm(n^2))
+  decomp <- qr(Z)
+  Q <- qr.Q(decomp)
+  R <- qr.R(decomp)
+  d <- diag(R)
+  ph <- d / abs(d)
+  O <- Q %*% diag(ph)
+  Z <- t(O) %*% diag(ev) %*% O
+  return(Z)
+}
+sigmaPD <- Posdef(n=8)
+
+pi_norm_corr= function(x) {
+  d=8
+  dmvnorm(x, mean = rep(0, d), sigma = sigmaPD, log = FALSE)
+}
+
+
+#plot(pi_norm_corr(cbind(seq(-5,5,0.1),seq(-5,5,0.1))))
+#values<-rmvnorm(n = 500,mean=rep(0,2),sigma=matrix(c(1,0.8,0.8,1),2,2))
+#plot(values[,1], values[,2])
+
 ############################################################################
 # Some common proposal distributions to draw from
 ############################################################################
-#define a multivariate normal proposal function
+
+#Multivariate Gaussian proposal distribution
+
 q_norm = function(mu, sigma) {
   mvrnorm(n = 1, mu = mu, Sigma = sigma)
 }
 
+
+
+############################################################################
+# Define a function to compute moving average acceptance rates
+############################################################################
+
+#moving_acceptance = function(n_moving, n_iter,X){
+#  moving_alpha = rep(NA, n_iter/n_moving)
+#  moving_alpha[1]= X$acceptances[n_moving]/n_moving
+#  for (i in seq(2*n_moving, n_iter, by=n_moving)) {
+#    moving_alpha[i/n_moving]=(X$acceptances[i]-X$acceptances[i-n_moving])/n_moving
+#  }
+#  return(moving_alpha)
+#}
+
+
 ############################################################################
 # Testing the MCMC function
 ############################################################################
-n_iter = 1000  #  Total number of iterations
-t_adapt = 100  #  Time step at which adaptation begins
-x_1 = rep(-10,20) #  Vector of inital values
-adapt = "AM2"  #  Choose the type of adaptation. "AM" or "None" currently.
-cov_estimator="standard"
+n_iter = 1000     #  Total number of iterations
+t_adapt = 600    #  Time step at which adaptation begins
+x_1 = rep(5,8)    #  Vector of inital values
+adapt = "AM"      #  Choose the type of adaptation. "AM" or "None" currently.
+cov_estimator="Sample covariance"    #  Choose the type of covariance matrix estimator. "Sample covariance", "Shrinkage estimator" or "Thresholding estimator".
 
-X = mcmc(target = pi_banana, n_iter = n_iter, x_1 = x_1, adapt=adapt, cov_estimator, t_adapt = t_adapt)
+X = mcmc(target = pi_norm_corr, n_iter = n_iter, x_1 = x_1, adapt=adapt, cov_estimator, t_adapt = t_adapt)
 
-#plot(X[,1],xlab="Iteration index", ylab="First coordinate of the AM markov chain")
-#hist(X[,1], xlab="First coordinate of the AM markov chain", ylab="Frequency", breaks=50, main="")
-plot(X[,2],X[,1],col='gold')
+plot1=plotIterations(X$X[,1], 1000, title="Values of the first MC component")
+
+
+
+iterations<-c(seq(from= 1, to=n_iter+1, by=1))  #costruct the sequence of iteration steps
+
+
+#moving_accep=plot(moving_acceptance(n_moving, n_iter, X))
+#n_moving=10
+
+
+
+############################################################################
+# Define functions to produce plots
+############################################################################
+
+plotTarget = function(target, d, num_values){
+  Z = matrix(cbind(rep(seq(from=(-10), to=10,length.out = 1000),d)), ncol=d)
+  functionValues = target(Z)
+  plot(seq(from=(-10), to=10,length.out = 1000),functionValues)
+}
+
+
+plotTarget(pi_norm_corr, 8, 100)
+#plotTarget(pi_banana_matrix, 2, 100)
+
+
+plotIterations = function(X, n_iter, title){
+  plot = qplot(seq(from= 1, to=n_iter+1, by=1), X ,geom="point",
+            main="",
+            xlab="Iteration", ylab=title, xlim=c(0,n_iter+1), alpha = I(1/1000), size=I(2.5)) +
+            theme(axis.title.x = element_text(size = 12), title = element_text(colour='black'),
+            axis.text.x=element_text(colour="black"), axis.text.y=element_text(colour="black"),
+            axis.line = element_line(colour="black", size = 1, linetype = "solid"))+
+            geom_point(size=2, shape=19, alpha=0.1)
+  return(plot)
+}
+
+
+plotComponents = function(X, Y , Xtitle, Ytitle){
+  plot = qplot(X, Y ,geom="point", main="", xlab=Xtitle, ylab=Ytitle, alpha = I(1/1000), size=I(2.5)) +
+    theme(axis.title.x = element_text(size = 12), title = element_text(colour='black'),
+          axis.text.x=element_text(colour="black"), axis.text.y=element_text(colour="black"),
+          axis.line = element_line(colour="black", size = 1, linetype = "solid"))+
+    geom_point(size=2, shape=19, alpha=0.1)
+  return(plot)
+}
+
+
+
+plotComponents = function(X, Y , Xtitle, Ytitle){
+  plot = qplot(X, Y ,geom="point", main="", xlab=Xtitle, ylab=Ytitle, alpha = I(1/1000), size=I(2.5)) +
+    theme(axis.title.x = element_text(size = 12), title = element_text(colour='black'),
+          axis.text.x=element_text(colour="black"), axis.text.y=element_text(colour="black"),
+          axis.line = element_line(colour="black", size = 1, linetype = "solid"))+
+    geom_point(size=2, shape=19, alpha=0.1)
+  return(plot)
+}
+
+plotACF = function(X, title){
+  plot = acf(X, main=title)
+}
+
+
+
+
+#plot1=plotIterations(X$X[,1], 5000, title="Values of the first MC component")
+#plot2=plotIterations(X$X[,2], 5000, title="Values of the second MC component")
+#grid.arrange(plot1, plot2, ncol=2)
+
+#plot3=plotComponents(X$X[,1], X$X[,2],  Xtitle = "First components of the MC", Ytitle = "Second components of the MC")
+#plot3
+
+#plot4=plotIterations(X$acceptance_rates, 5000, "Alpha")
+#plot4
+
+#plot5=plotIterations(X$sample_mean[,1], 5000, "Sample expected values")
+#plot5
+
+#plotACF(X$X[,1], "Autocorrelation function for the first MC component")
+
